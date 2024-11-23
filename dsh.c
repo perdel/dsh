d03fb03e0d674ed96bd1887372ecdc909ac1b050
Delwin Perera <perera@mm.tu-darmstadt.de>
2024-11-23 22:09:23 +0100

    Modularize main function
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

--- dsh.c
+++ dsh.c
@@ -6,14 +6,54 @@
 
 #define MAX_ARGS 1024
 
+char* read_input(char* buffer, size_t size) {
+    printf("$ ");
+    return fgets(buffer, size, stdin);
+}
+
+char** parse_command(char* input, char** args, int max_args) {
+    char* token = strtok(input, " \n");
+    int arg_count = 0;
+    while (token != NULL && arg_count < max_args) {
+        args[arg_count] = token;
+        token = strtok(NULL, " \n");
+        arg_count++;
+    }
+    args[arg_count] = NULL;
+    return args;
+}
+
+int execute_command(char** args) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            fprintf(stderr, "command not found: %s\n", args[0]);
+        }
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        int status;
+        if (wait(&status) == -1) {
+            perror("wait");
+        }
+        return WEXITSTATUS(status);
+    }
+}
+
+void handle_exit_status(int status) {
+    char exit_status_str[10];
+    snprintf(exit_status_str, sizeof(exit_status_str), "%d", status);
+    setenv("?", exit_status_str, 1);
+}
+
 int main() {
     char command[1024];
     char* args[MAX_ARGS];
     int status;
 
     while (1) {
-        printf("$ ");
-        if (fgets(command, sizeof(command), stdin) == NULL) {
+        if (read_input(command, sizeof(command)) == NULL) {
             if (feof(stdin)) {
                 // Exit shell when CTL+D is pressed
                 printf("\n");
@@ -25,38 +65,9 @@ int main() {
             }
         }
 
-        // Split the command into a command and arguments
-        char* token = strtok(command, " \n");
-        int arg_count = 0;
-        while (token != NULL && arg_count < MAX_ARGS) {
-            args[arg_count] = token;
-            token = strtok(NULL, " \n");
-            arg_count++;
-        }
-        args[arg_count] = NULL;
-
-        if (fork() == 0) {
-            // Child process
-            if (execvp(args[0], args) == -1) {
-                fprintf(stderr, "command not found: %s\n", args[0]);
-            }
-            exit(EXIT_FAILURE);
-        }
-
-        int last_exit_status = 0; 
-
-        // Parent process
-        if (wait(&status) == -1) {
-            perror("wait");
-        };
-
-        if (WIFEXITED(status)) {
-            last_exit_status = WEXITSTATUS(status);
-        }
-
-        char exit_status_str[10];
-        snprintf(exit_status_str, sizeof(exit_status_str), "%d", last_exit_status);
-        setenv("?", exit_status_str, 1);
+        parse_command(command, args, MAX_ARGS);
+        status = execute_command(args);
+        handle_exit_status(status);
     }
 
     return 0;
